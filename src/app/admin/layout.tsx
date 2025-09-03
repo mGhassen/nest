389ac62dadfa125fdd5337f1aba37e5d1703c670
Ghassen MENAOUAR <ghassen.menaouar@gmail.com>
2025-09-03 23:28:20 +0100

    Remove Admin Layout Component from Admin Module
    - Deleted the AdminLayout component from the admin layout file, streamlining the codebase and removing unused code.

--- src/app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import AdminLayout from "@/components/layout/admin-layout";
-
-export default function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <AdminLayout>{children}</AdminLayout>;
-}
